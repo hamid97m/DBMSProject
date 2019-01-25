973d78a75474cee695af81f3cbd5152e2f095397
hamid <shr.mahmoodi@gmail.com>
2019-01-25 15:56:20 +0330

    fix bug

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -31,27 +31,35 @@ public class Controller {
         TreeItem<String> rootItem = new TreeItem<String> ("سفارشات");
         rootItem.setExpanded(true);
         for (int i = 0; i <nodes.size(); i++) {
-            TreeItem<String> item = new TreeItem<String> (nodes.get(i).getOrder_date());
+            TreeItem<String> item = new TreeItem<String> (nodes.get(i).getOrder_id().toString());
             TreeItem<String> item1=new TreeItem<>();
             TreeItem<String> item2=new TreeItem<>();
             TreeItem<String> item3=new TreeItem<>();
             TreeItem<String> item4=new TreeItem<>();
             TreeItem<String> item5=new TreeItem<>();
-            for (int j = 0; j < 5; j++) {
-                item1 = new TreeItem<String> ("تاریخ تحویل  ="+nodes.get(i).getDelivery_date());
-                item2 = new TreeItem<String> ("شعبه  ="+nodes.get(i).getShop_bransh_id());
-                item3 = new TreeItem<String> ("هزینه حمل و نقل  ="+nodes.get(i).getTransport_cost());
-                item4 = new TreeItem<String> ("نام مشتری  ="+nodes.get(i).getCu_name());
-                item5 = new TreeItem<String> ("آدرس  ="+nodes.get(i).getCu_ad_city()+"  "+nodes.get(i).getCu_ad_street()+"  "+nodes.get(i).getShop_bransh_id());
+            TreeItem<String> item6=new TreeItem<>();
+            TreeItem<String> item7=new TreeItem<>();
+            TreeItem<String> item8=new TreeItem<>();
 
-            }
+                item6 = new TreeItem<String> (nodes.get(i).getOrder_date()+" = تاریخ خرید");
+                item1 = new TreeItem<String> (nodes.get(i).getDelivery_date()+" = تاریخ تحویل");
+                item2 = new TreeItem<String> (nodes.get(i).getShop_bransh_id()+" = شعبه");
+                item3 = new TreeItem<String> (nodes.get(i).getTransport_cost()+" = هزینه حمل و نقل");
+                item4 = new TreeItem<String> (nodes.get(i).getCu_name()+" = نام مشتری ");
+                item5 = new TreeItem<String> (nodes.get(i).getCu_ad_city()+" = شهر");
+                item7 = new TreeItem<String> (nodes.get(i).getCu_ad_building_num()+" = پلاک");
+                item8 = new TreeItem<String> (nodes.get(i).getCu_ad_street()+" = خیابان");
+
+
             item.getChildren().add(item1);
+            item.getChildren().add(item6);
             item.getChildren().add(item2);
             item.getChildren().add(item3);
             item.getChildren().add(item4);
             item.getChildren().add(item5);
+            item.getChildren().add(item7);
+            item.getChildren().add(item8);
             rootItem.getChildren().add(item);
-            item5 = new TreeItem<String> ("آدرس  ="+nodes.get(i).getCu_ad_city()+"  "+nodes.get(i).getCu_ad_street()+"  "+nodes.get(i).getShop_bransh_id());
 
 
         }
